4245de51fcac810554a5e002e4a31bfe08f2c935
vinnav <vincenzo.navarra@gmail.com>
2019-09-18 14:42:50 +0100

    solved leapYears

--- odin-exercises/XleapYears/leapYears.js
+++ odin-exercises/XleapYears/leapYears.js
@@ -1,4 +1,18 @@
-const leapYears = require('./leapYears')
+const leapYears = function(a) {
+    if((a % 4 == 0 && a % 100 !== 0) || a % 400 == 0){
+        return true;
+    }
+return false;
+}
+
+module.exports = leapYears
+
+
+/*
+
+Leap years are years divisible by four (like 1984 and 2004). 
+However, years divisible by 100 are not leap years (such as 1800 and 1900) 
+unless they are divisible by 400 (like 1600 and 2000, which were in fact leap years). 
 
 describe('leapYears', function() {
   it('works with non century years', function() {
@@ -20,3 +34,5 @@ describe('leapYears', function() {
     expect(leapYears(700)).toEqual(false);
   });
 });
+
+*/
