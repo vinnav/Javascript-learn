f8eaa6837a9c4b786b60eac70b67ab8d175c2c24
vinnav <vincenzo.navarra@gmail.com>
2019-09-25 17:32:47 +0100

    completed palindrom exercise

---
+++ odin-exercises/Xpalindromes/palindromes.js
@@ -0,0 +1,8 @@
+const palindromes = function(str) {
+    var re = /[\W_]/g;
+    var lowRegStr = str.toLowerCase().replace(re, '');
+    var reverseStr = lowRegStr.split('').reverse().join(''); 
+    return reverseStr === lowRegStr;
+}
+
+module.exports = palindromes
